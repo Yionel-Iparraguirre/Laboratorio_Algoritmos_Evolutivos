2f4765a43298befce1559eebc83cf570cb880f2a
Yionel-Iparraguirre <yionel.iparraguirre@gmail.com>
2025-05-20 00:22:48 -0500

    oño

---
+++ Ejercicio03.py
@@ -0,0 +1,6 @@
+print("==========================================")
+print("EJERCICIO 03 - USANDO EL DATASET LABDISTANCES")
+print("==========================================")
+import random
+import pandas as pd
+import numpy as np
